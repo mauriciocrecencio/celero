f42135d56f2bdf5dbffbddf035d6326c270773ea
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2020-12-09 22:04:45 -0300

    model and csv add

--- noc/models.py
+++ noc/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Noc(models.Model):
+    noc = models.CharField(max_length=4)
+    region = models.CharField(max_length=100)
+    team = models.CharField(max_length=100)
+    notes = models.CharField(max_length=255)
