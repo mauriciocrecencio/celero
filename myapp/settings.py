4c80a089a384ac7c3b3ed4457e7347a537deafbd
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2020-12-09 22:04:02 -0300

    olympics e noc add on settings

--- myapp/settings.py
+++ myapp/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'athletes'
+    'athletes',
+    'noc',
+    'olympics'
 ]
 
 MIDDLEWARE = [
