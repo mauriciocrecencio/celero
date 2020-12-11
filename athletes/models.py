f1f85744d3cac2a39dea217eefbccbc47feceedb
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2020-12-11 08:03:02 -0300

    model athlete and athlete_instance add

--- athletes/models.py
+++ athletes/models.py
@@ -1,19 +1,23 @@
 from django.db import models
-
-# Create your models here.
-
+from noc.models import Noc
+from events.models import Event
 class Athlete(models.Model):
+    GENDER_TYPES = [('M', 'M'),('F', 'F')]
     name = models.CharField(max_length=255)
-    gender = models.CharField(max_length=10)
+    gender = models.CharField(max_length=2, choices=GENDER_TYPES)
+    team = models.CharField(max_length=100)
+    noc = models.ForeignKey(Noc, on_delete=models.CASCADE)
+
+    
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super(Athlete, self).save(*args, **kwargs)
+
+class Athlete_instance(models.Model):
+    athlete_id = models.ForeignKey(Athlete, on_delete=models.CASCADE)
+    event_id = models.ManyToManyField(Event)
+    medal = models.CharField(max_length=20)
     age = models.IntegerField()
     height = models.CharField(max_length=100)
     weight = models.CharField(max_length=100)
-    team = models.CharField(max_length=100)
-    noc = models.CharField(max_length=100)
-    games = models.CharField(max_length=255)
-    year = models.IntegerField()
-    season = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    sport = models.CharField(max_length=255)
-    event = models.CharField(max_length=255)
-    medal = models.CharField(max_length=100)
