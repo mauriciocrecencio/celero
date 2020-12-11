76645d40d7a4972cfe665d8db0975ab20ddb4177
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2020-12-11 08:04:28 -0300

    get view for athlete

--- athletes/views.py
+++ athletes/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Athlete
+from .serializers import AthleteSerializer
 
 # Create your views here.
+class AthleteView(APIView):
+    def get(self,request):
+        queryset = Athlete.objects.all()
+        serializer = AthleteSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    # def post(self,request):
+    #     athlete = Athlete.objects.create(**kwargs)
+    #     serializer = AthleteSerializer(athlete)
+    #     return Response(serializer.data)
+
+    # # def patch(self,request):
+
+
+    # def delete(self, request, pk):
+    #     athlete = Athlete.objects.get(id=pk)
+    #     athlete.delete()
+    #     return Response(status=status.HTTP_200_OK)
